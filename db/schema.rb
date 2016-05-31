aed89fb633009819815fa63eef1a2a622f2875e1
Rooshy <ru_sha_nia@yahoo.ca>
2016-05-30 23:44:21 -0500

    Adding mailer  UserMailer.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160528044950) do
+ActiveRecord::Schema.define(version: 20160531043724) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20160528044950) do
     t.boolean  "confirmed",       default: false
     t.datetime "created_at",                      null: false
     t.datetime "updated_at",                      null: false
+    t.string   "confirm_token"
   end
 
 end
