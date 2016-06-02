044a2a1e41a6970a614128511dbeb807d33e0c89
Rooshy <ru_sha_nia@yahoo.ca>
2016-06-01 23:53:38 -0500

    Adding methods to posts controller

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -4,4 +4,26 @@ class Api:: PostsController < ApplicationController
 
   def index
     search_text = params[:search]
+    @posts = Post.all
+    if search_text.present?
+       @posts = @posts.search_all(params[:search])
+    end
   end
+
+  def authors
+    render json: {authors:  Post.pluck(:author).uniq.sort}
+  end
+
+  def show
+    @post = Post.find_by id: params[:id]
+  end
+
+  def create
+    @post = Post.new params.require(:post).permit(:author, :title, :content)
+    if @post.save
+      render :show
+    else
+      render json: @post.errors, status: 422
+  end
+ end
+end
